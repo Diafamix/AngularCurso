ad0c1c30bf56ca3885c0adf181e336cbb9ed2aae
DESKTOP-BR53GKA\Carlos <carloswow8@gmail.com>
2022-07-19 01:41:05 +0200

    creacion de ckliente.json.ts

---
+++ src/app/clientes/clientes.json.ts
@@ -0,0 +1,9 @@
+import { Cliente } from './cliente';
+
+export const CLIENTES: Cliente[] = [
+    {id: 1, nombre: 'Carlos', apellido: 'Cueva',email: 'carloswow8@gmail.com',createAt: '2022-07-19'},
+    {id: 2, nombre: 'Carlos2', apellido: 'Cueva',email: 'carloswow8@gmail.com',createAt: '2022-07-19'},
+    {id: 3, nombre: 'Carlos3', apellido: 'Cueva',email: 'carloswow8@gmail.com',createAt: '2022-07-19'},
+    {id: 4, nombre: 'Carlos4', apellido: 'Cueva',email: 'carloswow8@gmail.com',createAt: '2022-07-19'}, 
+    {id: 5, nombre: 'Carlos5', apellido: 'Cueva',email: 'carloswow8@gmail.com',createAt: '2022-07-19'}
+  ];
